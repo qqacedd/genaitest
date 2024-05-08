dc63e59d0f04643a1eb05d95852ab5244a48dcb0
qqacedd <165684496+qqacedd@users.noreply.github.com>
2024-05-08 22:29:35 +0000

    GenAIImageDemo

---
+++ sbdemo0109/Program.cs
@@ -0,0 +1,14 @@
+﻿using OpenAI_API;
+
+// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+
+var api = new OpenAIAPI("key");
+
+var request = new OpenAI_API.Images.ImageGenerationRequest
+{
+    Prompt = "A cute kitten in a basket"
+};
+
+var response = api.ImageGenerations.CreateImageAsync(request);
+Console.WriteLine("Image URL: {0}", response.Data[0].Url);
